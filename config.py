b878787dff68d74a0ad852eda97f2ae6905ae010
Aleien <technogenom@gmail.com>
2025-05-30 21:16:25 +0300

    Removed wrong files

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
+import os
 import re
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
+
+
+DOTENV = os.path.join(os.path.dirname(__file__), ".env")
 
 
 class Settings(BaseSettings):
@@ -13,7 +17,7 @@ class Settings(BaseSettings):
     # через model_config
     # В данном случае будет использоваться файла .env, который будет прочитан
     # с кодировкой UTF-8
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file=DOTENV, env_file_encoding='utf-8')
 
 
 # При импорте файла сразу создастся
